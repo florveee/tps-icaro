f27f7cd2bf1151c7dfeeb5d06a95c42d6e170caa
florveee <123824856+florveee@users.noreply.github.com>
2023-05-05 16:58:39 -0300

    Add files via upload

---
+++ tp1/tp1.js
@@ -0,0 +1,15 @@
+//TP 1
+//Crear una carpeta en tu pc dentro de ella iniciar git
+//Configurar tu user.name y tu user.email para esta carpeta
+//Crear un archivo javascript, con cualquier nombre
+//Inicia dos variables numéricas y asignales cualquier valor numérico
+//Suma las variables creadas en el punto anterior y guarda el resultado en una tercara variable llamada resultado
+//Muestra la variable resultado en consula
+//Crea un repositorio en Github y sube tu código, luego comparte el link de tu repositorio
+
+a = 20
+b = 40
+
+resultado = a + b 
+
+console.log(resultado)
